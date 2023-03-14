99530353832c9600835cc6c9c151ee661909c908
rayniel95 <rainyel.ramos@gmail.com>
2023-03-14 14:50:23 -0400

    initial commit

---
+++ src/components/TodoTask.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default{
+  data(){
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
