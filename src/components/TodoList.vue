269852bc9065f57df4409f923ac8a4392f6cfd8d
rayniel95 <rainyel.ramos@gmail.com>
2023-03-14 16:59:52 -0400

    base list component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,34 +1,26 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts">
+import TodoTask from "./TodoTask.vue";
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+export default {
+  components:{
+    TodoTask
+  },
+  data(){
+    return {
+      todos: []
+    }
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+    <ul>
+      <li v-for="todo in todos">
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
