f6dcd4ef277454d5d6a869d4fff9149faac06dd4
rayniel95 <rainyel.ramos@gmail.com>
2023-03-15 23:11:15 -0400

    add comment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //TODO - use this just for production in gh-pages
   base: '/todo-block-frontend/'
 })
